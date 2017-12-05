c6e87a1a6b6013e997d769483effeb163bcc7f23
HamzaPython <hamzajamal.fsr@gmail.com>
2017-12-05 17:14:11 +0000

    I modified the split_text code in way that the spliting methodes take text like argument

--- split_text.py
+++ split_text.py
@@ -1,29 +1,28 @@
 from nltk.tokenize import regexp_tokenize, wordpunct_tokenize
 
 
-class split_text():
-    def __init__(self, text):
-        self.text = text
+class split_text_class():
+    def spliting_sentence(self, text=''):
+        ''' this method is used to split just 1 sentense '''
+        return wordpunct_tokenize(text)
 
-    def spliting_sentence(self):
-        ''' this method is used to split just 1 sentense'''
-        return wordpunct_tokenize(self.text)
-
-    def spliring_text(self):
+    def spliting_text(self, text=''):
         ''' this method is used to split more than 1 sentense (text)'''
         pattern = r'[^\n\t\;\:\?\!\,\.]+'
-        sentenses = regexp_tokenize(
-            self.text, pattern)  # here we split the text to multiple sentenses
+
+        # here we will split the text to multiple sentenses:
+        sentenses = regexp_tokenize(text, pattern)
+
+        # here we will split each sentence to multiple words:
         words = list(
             map(lambda sentense: wordpunct_tokenize(sentense), sentenses))
-        # here we split each sentence to multiple words
         return words
 
     ''' this 2 methods (str and repr) are
     used in this code to increase readability '''
 
     def __str__(self):
-        return "this is an object from split_text class"
+        return "this is an object from split_text_class class"
 
     def __repr__(self):
-        return "split_text({0.text})".format(self)
+        return "split_text_class({0.text})".format(self)
