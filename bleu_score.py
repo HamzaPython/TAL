4c9090244339fd4b599f45947ca3ed45d8d7c210
HamzaPython <hamzajamal.fsr@gmail.com>
2017-12-09 11:14:53 +0000

    somethings I have to do in bleu_score code

--- bleu_score.py
+++ bleu_score.py
@@ -56,6 +56,15 @@ class bleu_score_class():
             return 0
 
 
+# -------------------------------Exemples:-----------------------------
+# ----------------------------------1----------------------------------
+# b = bleu_score_class('my name', 'my name is hamza')
+# print(b.calculate_bleu_score())
+# ------> I have to add Smoothing, multibleu and auto reweigh techniques
+# ----------------------------------2----------------------------------
 # b = bleu_score_class('my name is hamza jamil.', 'my name is hamza jamal,',
 #                      'my name is hamza jamalo.')
 # print(b.calculate_bleu_score())
+# ------> It works good
+# ----------------------------------3----------------------------------
+#
