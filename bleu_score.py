5ae7e46e3858bed9a01f0d70a5f6d4330fa79406
HamzaPython <hamzajamal.fsr@gmail.com>
2017-12-11 11:56:06 +0000

    I moved some code from bleu_score.py to split_text.py

--- bleu_score.py
+++ bleu_score.py
@@ -3,57 +3,29 @@ from split_text import split_text_class
 
 
 class bleu_score_class():
-    _p = split_text_class(
-    )  # this is an object for spliting all texts that we need it to split
-
     def __init__(self, hypothese='', *references):
         ''' each bleu_score_class object has hypothese and references '''
         self.references = references
         self.hypothese = hypothese
+        self._p = split_text_class(hypothese, references)
+        # _p is a private attribut for spliting
+        # all texts that we need it to split
 
     def calculate_bleu_score(self):
         ''' this is the main method to calculate the bleu score '''
-        nb_references = len(self.references)
-        # calculate the number of reference
-        if nb_references > 0:
-            lenght = len(bleu_score_class._p.spliting_text(self.references[0]))
-            # calculate the number of sentences
-            if lenght == 1:
-                ''' 1 or more references with 1 sentence: '''
-                ref = []
-                for reference in self.references:
-                    # split each one of the references
-                    splited_reference = bleu_score_class._p.spliting_sentence(
-                        reference)
-                    ref.append(splited_reference)
-                hypo = bleu_score_class._p.spliting_sentence(self.hypothese)
-                # split the hypothese
-                bleu_score = sentence_bleu(ref, hypo)
-                # calculate the sentence-level bleu score
-                return bleu_score
-            elif lenght > 1:
-                ''' 1 or more references with more than 1 sentence: '''
-                ref = []
-                for reference in self.references:
-                    # split each one of the references
-                    splited_reference = bleu_score_class._p.spliting_text(
-                        reference)
-                    ref.append(splited_reference)
-                ordered_references = zip(*ref)
-                ordered_references = list(map(list, ordered_references))
-                # here we sort the sentences in way that each sentence
-                # of hypothese have 1 or more reference sentences
-                hypo = bleu_score_class._p.spliting_text(self.hypothese)
-                # split the hypothese
-                bleu_score = corpus_bleu(ordered_references, hypo)
-                # calculate the corpus-level bleu score
-                return bleu_score
-            else:
-                # references = ''
-                return 0
-        else:
-            # references = []
+        hypo, ref, has_one_sentence = self._p.split_references_hypothesis()
+        if ref is None:
             return 0
+        elif has_one_sentence:
+            ''' 1 or more references with 1 sentence: '''
+            bleu_score = sentence_bleu(ref, hypo)
+            # calculate the sentence-level bleu score
+            return bleu_score
+        else:
+            ''' 1 or more references with more than 1 sentence: '''
+            bleu_score = corpus_bleu(ref, hypo)
+            # calculate the corpus-level bleu score
+            return bleu_score
 
 
 # -------------------------------Exemples:-----------------------------
@@ -62,8 +34,8 @@ class bleu_score_class():
 # print(b.calculate_bleu_score())
 # ------> I have to add Smoothing, multibleu and auto reweigh techniques
 # ----------------------------------2----------------------------------
-# b = bleu_score_class('my name is hamza jamil.', 'my name is hamza jamal,',
-#                      'my name is hamza jamalo.')
+# b = bleu_score_class('my name is hamza jamal',
+#                      'my name is hamza jamali,', 'my name is hamza jamalo.')
 # print(b.calculate_bleu_score())
 # ------> It works good
 # ----------------------------------3----------------------------------
--- split_text.py
+++ split_text.py
@@ -2,6 +2,11 @@ from nltk.tokenize import regexp_tokenize, wordpunct_tokenize
 
 
 class split_text_class():
+    def __init__(self, hypothese='', references=''):
+        ''' each metrics has an hypothese and references '''
+        self.references = references
+        self.hypothese = hypothese
+
     def spliting_sentence(self, text=''):
         ''' this method is used to split just 1 sentense '''
         return wordpunct_tokenize(text)
@@ -19,6 +24,44 @@ class split_text_class():
             map(lambda sentense: wordpunct_tokenize(sentense), sentenses))
         return words
 
+    def split_references_hypothesis(self):
+        ''' this is the main method that gives the metrics their parameters'''
+        nb_references = len(self.references)
+        # calculate the number of reference
+        if nb_references > 0:
+            lenght = len(self.spliting_text(self.references[0]))
+            # calculate the number of sentences
+            if lenght == 1:
+                ''' 1 or more references with 1 sentence: '''
+                ref = []
+                for reference in self.references:
+                    # split each one of the references
+                    splited_reference = self.spliting_sentence(reference)
+                    ref.append(splited_reference)
+                hypo = self.spliting_sentence(self.hypothese)
+                # split the hypothese
+                return hypo, ref, isinstance(hypo[0], str)
+            elif lenght > 1:
+                ''' 1 or more references with more than 1 sentence: '''
+                ref = []
+                for reference in self.references:
+                    # split each one of the references
+                    splited_reference = self.spliting_text(reference)
+                    ref.append(splited_reference)
+                ordered_references = zip(*ref)
+                ordered_references = list(map(list, ordered_references))
+                # here we sort the sentences in way that each sentence
+                # of hypothese have 1 or more reference sentences
+                hypo = self.spliting_text(self.hypothese)
+                # split the hypothese
+                return hypo, ref, isinstance(hypo[0], str)
+            else:
+                # references = ''
+                return self.hypothese, None, False
+        else:
+            # references = []
+            return self.hypothese, None, False
+
     ''' this 2 methods (str and repr) are
     used in this code to increase readability '''
 
@@ -27,3 +70,10 @@ class split_text_class():
 
     def __repr__(self):
         return "split_text_class({0.text})".format(self)
+
+
+# p = split_text_class('my name is hamza jamil.', 'my name is hamza jamilo.')
+# hypo, ref, has_one_sentence = p.split_references_hypothesis()
+# print(hypo)
+# print(ref)
+# print(has_one_sentence)
